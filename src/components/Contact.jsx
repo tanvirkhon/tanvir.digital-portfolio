5c501ff0f13a15a36f4a5de23f3cb688d3906a21
Hoard Coin <119143763+tanvirkhon@users.noreply.github.com>
2023-03-08 16:41:55 -0600

    Added email functionality using Email JS

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,8 +22,49 @@ const Contact = () => {
   const [loading, setLoading] = useState(false);
 
   // Handle form functions
-  const handleChange = (e) => {};
-  const handleSubmit = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        "service_nlhtp5f",
+        "template_8b6ijmc",
+        {
+          from_name: form.name,
+          to_name: "Tanvir",
+          from_email: form.email,
+          to_email: "tanvirkhondakar@gmail.com",
+          message: form.message,
+        },
+        "BjzNb3RGhP3kwl3P5"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert(
+            "Thanks for contacting me! I will get back to you as soon as possible."
+          );
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.log(error);
+          alert("Oops! Something went wrong. Please try again.");
+        }
+      );
+  };
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
